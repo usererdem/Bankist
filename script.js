02091e2e0024b6b8a20431116b96af641e712678
Erdem Elvan <erdemelvan35@gmail.com>
2022-07-14 18:39:50 +0300

    display summary with the help of array methods

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ function displayMovements(movements) {
           <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-          <div class="movements__value">${mov}</div>
+          <div class="movements__value">${mov}€</div>
     </div>
     `;
 
@@ -78,21 +78,40 @@ displayMovements(account1.movements);
 
 function calcDisplayBalance(movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR` 
-};
+  labelBalance.textContent = `${balance}€`;
+}
 calcDisplayBalance(account1.movements);
 
+function calcDisplaySummary(movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`
+}
+calcDisplaySummary(account1.movements);
+
 function createUsernames(accs) {
-  accs.forEach(function(acc){
+  accs.forEach(function (acc) {
     acc.username = acc.owner
       .toLowerCase()
       .split(' ')
       .map(name => name[0])
       .join('');
   });
-};
-createUsernames(accounts)
-
-
-
-
+}
+createUsernames(accounts);
