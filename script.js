894f0fe7557db8a879ca5bdc834caecf39853fb7
Erdem Elvan <erdemelvan35@gmail.com>
2022-08-22 16:32:13 +0300

    close account functionality

--- script.js
+++ script.js
@@ -122,9 +122,9 @@ function updateUI(acc) {
   calcDisplayBalance(acc);
   // Display summary
   calcDisplaySummary(acc);
-};
+}
 
-// Event handler
+// Event handlers
 let currentAccount;
 
 btnLogin.addEventListener('click', function (e) {
@@ -173,4 +173,23 @@ btnTransfer.addEventListener('click', function (e) {
     // Update UI
     updateUI(currentAccount);
   }
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
 });
