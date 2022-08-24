f2043e35e6ce24f53b6941edb2d3860e3aae077a
Erdem Elvan <erdemelvan35@gmail.com>
2022-08-24 20:20:21 +0300

    Creating an array for deposit withdrawal values, it is for test purposes

--- script.js
+++ script.js
@@ -218,3 +218,13 @@ btnSort.addEventListener('click', function (e) {
   displayMovements(currentAccount.movements, !sorted);
   sorted = !sorted;
 });
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+});
